d73536420c63ed88cc016ddd0bbfea683090b656
Raj <rajk200136@gmail.com>
2026-05-18 17:06:53 +0530

    Updated AI image generator UI Now it become responsive

--- src/Components/ImageGenerator/ImageGenerator.jsx
+++ src/Components/ImageGenerator/ImageGenerator.jsx
@@ -7,10 +7,12 @@ export const ImageGenerator = () => {
   const inputRef = useRef(null);
 
   const [image_url, setImage_url] = useState("/");
+  const [loading, setLoading] = useState(false);
 
   const generateImage = async () => {
 
     if (inputRef.current.value === "") return;
+    setLoading(true);
 
     const imageUrl = `https://image.pollinations.ai/prompt/${encodeURIComponent(
       inputRef.current.value
@@ -30,7 +32,12 @@ export const ImageGenerator = () => {
         <img
           src={image_url === "/" ? defaultImage : image_url}
           alt="Generated"
+          onLoad={() => setLoading(false)}
         />
+      </div>
+      <div className="loading">
+        <div className={loading?"loading-bar-full":"loading-bar"}></div>
+        <div className={loading?"loading-text":"display-none"}>Loading...</div>
       </div>
 
       <div className='search-box'>
